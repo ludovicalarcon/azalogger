846fc6284ab3a2901b1a635bb3cf399707bdec06
Ludovic Alarcon <alarcon.ludovic@gmail.com>
2025-08-05 20:46:42 +0900

    feat(tests): add unit test for factory

---
+++ factory_test.go
@@ -0,0 +1,42 @@
+package azalogger
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewLogger(t *testing.T) {
+	t.Run("should create zap logger based on backend", func(t *testing.T) {
+		cfg := Config{
+			Backend: ZapBackend,
+		}
+
+		logger, err := NewLogger(cfg)
+		require.NoError(t, err)
+		require.NotNil(t, logger)
+		assert.IsType(t, &zapLogger{}, logger)
+	})
+
+	t.Run("should create in-memory logger based on backend", func(t *testing.T) {
+		cfg := Config{
+			Backend: InMemoryBackend,
+		}
+
+		logger, _ := NewLogger(cfg)
+		require.NotNil(t, logger)
+		assert.IsType(t, &InMemoryLogger{}, logger)
+	})
+
+	t.Run("should return an error on unsupported backend", func(t *testing.T) {
+		cfg := Config{
+			Backend: 1000,
+		}
+
+		logger, err := NewLogger(cfg)
+		require.Error(t, err)
+		assert.EqualError(t, err, ErrUnsupportedBackend.Error())
+		assert.Nil(t, logger)
+	})
+}
